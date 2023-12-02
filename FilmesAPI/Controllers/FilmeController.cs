c5aa4da789125ab3b5344fbc7a5d1f0d025fd854
Vinicius Rosa <84021144+ViniciusRosa1@users.noreply.github.com>
2023-12-02 09:57:08 -0300

    Add files via upload

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,37 @@
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+    [HttpPost]
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
+    {    
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperarFilmePorId), 
+        new { id = filme.Id}, filme);
+    }
+
+    
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilme([FromQuery]int skip = 0, [FromQuery]int take= 50){
+        return filmes.Skip(skip).Take(take);    
+    }
+    [HttpGet("{id}")]
+    public IActionResult RecuperarFilmePorId(int id){
+       var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+       if (filme == null) return NotFound();
+       return Ok(filme);
+    }
+}
+
